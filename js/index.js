66f2454cb2f242a6a5d2cd975ed0592a69fb002c
George Lopez <50560354+GeorgeChristopherLopez@users.noreply.github.com>
2019-08-19 19:52:17 -0400

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,38 @@
+window.sr = ScrollReveal();
+
+
+sr.reveal('#navbarCollapse', {
+    duration: 3000,
+    distance:"1000px",
+    origin: 'left'
+});
+sr.reveal('.fadeIn', {
+    duration: 2000
+
+});
+
+sr.reveal('#contact-btn', {
+    duration: 2000,
+    delay: 4000
+
+});
+
+
+sr.reveal('.fadeInLeft', {
+    duration: 2000,
+    distance: "1000px",
+    origin: 'left'
+});
+
+
+sr.reveal('.fadeInRight', {
+    duration: 2000,
+    distance: "1000px",
+    origin: 'right'
+});
+
+sr.reveal('.fadeUp', {
+    duration: 2000,
+    distance: "50px",
+    origin: 'bottom'
+});
